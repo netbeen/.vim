ca6d7654c4169f28700de87fe0613da4326afb31
NetBeen <netbeen.cn@gmail.com>
2015-09-13 19:28:46 +0800

    Add install.sh & pathogen

---
+++ install.sh
@@ -0,0 +1,8 @@
+cd bundle
+
+git clone https://github.com/tpope/vim-pathogen.git
+cp vim-pathogen/autoload/pathogen.vim ../autoload
+
+git clone https://github.com/scrooloose/nerdtree.git
+
+git clone https://github.com/Lokaltog/vim-powerline.git
