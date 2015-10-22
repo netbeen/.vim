54d18bdf382290acd662b17262a0223404ed5d2d
NetBeen <netbeen.cn@gmail.com>
2015-10-22 20:11:00 +0800

    新增Node.js支持

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ cp vim-pathogen/autoload/pathogen.vim ../autoload
 git clone https://github.com/scrooloose/nerdtree.git
 
 git clone https://github.com/Lokaltog/vim-powerline.git
+
+git clone https://github.com/moll/vim-node.git ~/.vim/bundle/node  #安装Node.js插件
+
