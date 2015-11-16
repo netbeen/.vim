eaf3a40f42d3739b24cb863e1b1b578d762c1073
NetBeen <netbeen.cn@gmail.com>
2015-11-16 09:52:18 +0800

    在clone插件之前先删除对应的文件夹，防止已有文件的干扰

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 cd bundle
 
+rm -rf vim-pathogen/
 git clone https://github.com/tpope/vim-pathogen.git
 cp vim-pathogen/autoload/pathogen.vim ../autoload
 
+rm -rf nerdtree/
 git clone https://github.com/scrooloose/nerdtree.git
 
+rm -rf vim-powerline
 git clone https://github.com/Lokaltog/vim-powerline.git
 
+rm -rf node
 git clone https://github.com/moll/vim-node.git ~/.vim/bundle/node  #安装Node.js插件
 
